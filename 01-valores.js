0387af9286034d06028076ad7cf02109838299fd
FranciscoAriza <franciscoariza724@gmail.com>
2021-07-21 14:54:03 -0500

    first commit
    Data types in JavaScript Primitives and Objects

---
+++ 01-valores.js
@@ -0,0 +1,24 @@
+//Primitive data types
+
+40 //Number. In JavaScript, Number is a numeric data type in the double-precision 64-bit floating point format (IEEE 754)
+
+"Some text"   //String. In any computer programming language, a string is a sequence of characters used to represent text.
+
+/*Boolean. In computer science, a Boolean is a logical data type that can have only the values true or false. 
+For example, in JavaScript, Boolean conditionals are often used to decide which sections of code to execute (such as in if statements) or repeat (such as in for loops). */
+true
+false
+
+null    //Null, In JavaScript, null is marked as one of the primitive values, because its behavior is seemingly primitive.
+
+undefined //Undefined. undefined is a primitive value automatically assigned to variables that have just been declared, or to formal arguments for which there are no actual arguments.
+
+
+/* ====================================================================== */
+
+//Objects
+
+[1,2,3,4]   //arrays
+{name: "Luis"}  //Keys // objects
+
+//You can found more documentation in https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
