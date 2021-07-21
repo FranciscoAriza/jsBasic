342c493f9cac710523b14a340b2cfaaa9d6582ed
FranciscoAriza <franciscoariza724@gmail.com>
2021-07-21 15:20:39 -0500

    include variables file

---
+++ 02-Variables.js
@@ -0,0 +1,28 @@
+// A variable is a place in the computer memory where we can save data.
+
+var variable = "Some data"  
+
+// We use var to indicate that we go to declare a variable
+
+// We use = to assign some data (value) to the variable.
+
+// How to declare a variable?
+
+var age;
+
+//How initiate a variable?
+
+age = 36;
+
+// We can declare an initiate a variable in the same instruction
+
+var age = 36;
+
+// Some examples with variables of type object
+
+var elements = ['Computer', 'Cellphone']
+
+var person = {
+    name: 'Paula',
+    age: 23
+}
